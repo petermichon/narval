8506924ba8aa9f9af29d9c015ab7675cc5f0fc53
Peter Michon <peter.michon@proton.me>
2025-06-29 15:15:39 +0200

    Move routes to separate function

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,20 @@
 import { DefaultMap } from "./default-map/mod.ts";
 
+type Handler = (args?: any) => any;
+
+type Result = {
+  content: any;
+  type: string;
+  status: number;
+};
+
 enum HTTPMethod {
   GET = "GET",
+}
+
+enum Status {
+  OK = 200,
+  NOT_FOUND = 404,
 }
 
 enum MimeType {
@@ -22,152 +35,16 @@ let nbRequests = 0;
 export function handler(req: Request) {
   const url = new URL(req.url);
 
-  // console.log(url.pathname);
+  // get method
+  const method = req.method as HTTPMethod;
+  console.log(method);
 
-  type Handler = (args?: any) => any;
+  // console.log(url.pathname);
 
   const routes = new DefaultMap<any, Handler>();
 
-  type Result = {
-    content: any;
-    type: string;
-    status: number;
-  };
-
-  {
-    routes.set("/", (): Result => {
-      // Metrics
-      nbRequests += 1;
-      console.log(`Query: ${nbRequests}`);
-
-      const path = "./website/index.html";
-      const result = {
-        type: MimeType.HTML,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/tailwind.css", (): Result => {
-      const path = "./website/tailwind.css";
-      const result = {
-        type: MimeType.CSS,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/main.js", (): Result => {
-      const path = "./website/main.js";
-      const result = {
-        type: MimeType.JavaScript,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/site.webmanifest", (): Result => {
-      const path = "./website/site.webmanifest";
-      const result = {
-        type: MimeType.WebManifest,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/android-chrome-192x192.png", (): Result => {
-      const path = "./website/media/android-chrome-192x192.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    // routes.set("/android-chrome-512x512.png", (): Result => {
-    //   const path = "./website/media/android-chrome-512x512.png";
-    //   const result = {
-    //     type: MimeType.PNG,
-    //     content: Deno.readFileSync(path),
-    //     status: 200,
-    //   };
-    //   return result;
-    // });
-
-    routes.set("/screenshot-wide.png", (): Result => {
-      const path = "./website/media/screenshot-wide.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/screenshot-narrow.png", (): Result => {
-      const path = "./website/media/screenshot-narrow.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    // routes.set("/favicon.ico", (): Result => {
-    //   const path = "./website/media/favicon.ico";
-    //   const result = {
-    //     type: MimeType.Icon,
-    //     content: Deno.readFileSync(path),
-    //     status: 200,
-    //   };
-    //   return result;
-    // });
-
-    routes.set("/narval.png", (): Result => {
-      const path = "./website/media/narval.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/favicon-32x32.png", (): Result => {
-      const path = "./website/media/favicon-32x32.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.set("/favicon-32x32.png", (): Result => {
-      const path = "./website/media/favicon-32x32.png";
-      const result = {
-        type: MimeType.PNG,
-        content: Deno.readFileSync(path),
-        status: 200,
-      };
-      return result;
-    });
-
-    routes.default((): Result => {
-      const result = {
-        type: MimeType.Text,
-        content: "Not Found",
-        status: 404,
-      };
-      return result;
-    });
-  }
+  // LOAD ROUTES
+  loadRoutes(routes);
 
   const handler = routes.get(url.pathname) as Handler;
   const result = handler();
@@ -179,4 +56,141 @@ export function handler(req: Request) {
   });
 
   return response;
+}
+
+function loadRoutes(routes: DefaultMap<any, Handler>) {
+  routes.set("/index.html", (): Result => {
+    // Metrics
+    nbRequests += 1;
+    console.log(`Query: ${nbRequests}`);
+
+    const path = "./website/index.html";
+    const result = {
+      type: MimeType.HTML,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/", routes.get("/index.html") as Handler);
+
+  routes.set("/tailwind.css", (): Result => {
+    const path = "./website/tailwind.css";
+    const result = {
+      type: MimeType.CSS,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/main.js", (): Result => {
+    const path = "./website/main.js";
+    const result = {
+      type: MimeType.JavaScript,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/site.webmanifest", (): Result => {
+    const path = "./website/site.webmanifest";
+    const result = {
+      type: MimeType.WebManifest,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/android-chrome-192x192.png", (): Result => {
+    const path = "./website/media/android-chrome-192x192.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  // routes.set("/android-chrome-512x512.png", (): Result => {
+  //   const path = "./website/media/android-chrome-512x512.png";
+  //   const result = {
+  //     type: MimeType.PNG,
+  //     content: Deno.readFileSync(path),
+  //     status: Status.OK,
+  //   };
+  //   return result;
+  // });
+
+  routes.set("/screenshot-wide.png", (): Result => {
+    const path = "./website/media/screenshot-wide.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/screenshot-narrow.png", (): Result => {
+    const path = "./website/media/screenshot-narrow.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  // routes.set("/favicon.ico", (): Result => {
+  //   const path = "./website/media/favicon.ico";
+  //   const result = {
+  //     type: MimeType.Icon,
+  //     content: Deno.readFileSync(path),
+  //     status: Status.OK,
+  //   };
+  //   return result;
+  // });
+
+  routes.set("/narval.png", (): Result => {
+    const path = "./website/media/narval.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/favicon-32x32.png", (): Result => {
+    const path = "./website/media/favicon-32x32.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.set("/favicon-32x32.png", (): Result => {
+    const path = "./website/media/favicon-32x32.png";
+    const result = {
+      type: MimeType.PNG,
+      content: Deno.readFileSync(path),
+      status: Status.OK,
+    };
+    return result;
+  });
+
+  routes.default((): Result => {
+    const result = {
+      type: MimeType.Text,
+      content: "Not Found",
+      status: Status.NOT_FOUND,
+    };
+    return result;
+  });
 }
