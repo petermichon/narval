07ca1ee099f5329f7d0be033f77e495cdbfd266c
Peter Michon <peter.michon@proton.me>
2025-07-03 22:12:17 +0200

    Add tailwind.config.js

---
+++ src/website/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        // roboto: ["Roboto", "sans-serif"],
+        // roboto-variable: ["Roboto Variable", "sans-serif"],
+      },
+    },
+  },
+};
