06860cdb150a3c65cd54659d9a1c82cd3d4792fc
Peter Michon <peter.michon@proton.me>
2025-07-16 21:14:04 +0200

    Connect feed page to database

--- src/website/main.ts
+++ src/website/main.ts
@@ -5,39 +5,13 @@ export {}
 import { newFeedElement } from './feed.ts'
 import { newVideoElement } from './video.ts'
 
-const videos = [
-  { id: 'OvL42CcOt-4', time: '0' },
-  { id: 'h9QHarUTy9w', time: '2599' },
-  { id: 'wSa-sjaLVOM', time: '3507' },
-  { id: 'wrFsapf0Enk', time: '4280' },
-  { id: 'ssSRJ5cXn20', time: '1342' },
-  { id: 'hV_2UC_6bTU', time: '0' },
-  { id: 'TqwdGyfewTs', time: '0' },
-  { id: 'Qe5WT22-AO8', time: '0' },
-  { id: 'xuHVTYekC9M', time: '0' },
-  { id: 'tYEflPGA1YM', time: '0' },
-  { id: 'dxCQUgYEiM4', time: '0' },
-  { id: 'yd4KskruaPM', time: '0' },
-  { id: '9tX2BOCgJH0', time: '0' },
-  { id: 'm7TUewww3fo', time: '0' },
-  { id: '37Z7yKvzVaw', time: '0' },
-  { id: '6MRu33HG0Io', time: '0' },
-  { id: 'NuDhD6-4ZQI', time: '0' },
-
-  { id: 'AjWfY7SnMBI', time: '0' },
-  { id: 'QggJzZdIYPI', time: '0' },
-  { id: '4zUUCTwOb0w', time: '0' },
-  { id: '9sCuyYOo3tQ', time: '0' },
-]
+let videos: { id: string; time: string }[] = []
 
 const pathname = globalThis.location.pathname
 // console.log(pathname)
 
 goToPage(pathname)
 
-/**
- * @param {string} pathname
- */
 function goToPage(pathname: string) {
   const app = document.getElementById('app')
 
@@ -46,18 +20,26 @@ function goToPage(pathname: string) {
   }
 
   if (pathname === '/') {
-    document.title = 'Narval'
-    const feedElement = newFeedElement(videos)
+    fetch('http://localhost:3000')
+      .then((response) => response.json())
+      .then((data) => {
+        videos = data
 
-    feedElement.addEventListener('video-click', (event) => {
-      // @ts-ignore: <>
-      const video = event.detail.video
-      history.pushState({}, '', `/video?v=${video.id}&t=${video.time}`)
-      goToPage('/video')
-    })
+        // console.log(videos)
 
-    app.innerHTML = ''
-    app.appendChild(feedElement)
+        document.title = 'Narval'
+        const feedElement = newFeedElement(videos)
+
+        feedElement.addEventListener('video-click', (event) => {
+          // @ts-ignore: <>
+          const video = event.detail.video
+          history.pushState({}, '', `/video?v=${video.id}&t=${video.time}`)
+          goToPage('/video')
+        })
+
+        app.innerHTML = ''
+        app.appendChild(feedElement)
+      })
   }
 
   if (pathname === '/feed') {
