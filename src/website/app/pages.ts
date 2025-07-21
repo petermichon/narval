b42668a8c28f1493f1abfb51c032c7e72b45bf25
Peter Michon <peter.michon@proton.me>
2025-07-21 15:50:03 +0200

    Update .prettierignore and pages.ts

--- src/website/app/pages.ts
+++ src/website/app/pages.ts
@@ -16,102 +16,100 @@ function goToPage(pathname: string) {
     throw new Error('app not found')
   }
 
-  const dmap = new DefaultMap()
+  const dmap = new DefaultMap<string, () => void>()
 
-  {
-    dmap.set('/', () => {
-      document.title = 'Narval'
+  dmap.set('/', () => {
+    document.title = 'Narval'
 
-      fetch('videos.json')
-        .then((response) => response.json())
-        .then((data) => {
-          videos = data
+    fetch('videos.json')
+      .then((response) => response.json())
+      .then((data) => {
+        videos = data
 
-          // console.log(videos)
-          const topBar = newTopBar()
-          const feedElement = newFeedElement(videos)
-          const footer = document.createElement('div')
-          footer.className = 'h-20 bg-white dark:bg-neutral-900'
-          const bottomNavBar = document.createElement('nav')
-          bottomNavBar.className =
-            'absolute w-full p-7 fixed bottom-0 left-0 right-0 lg:hidden z-2 opacity-90 backdrop-blur-md bg-gray-100 dark:bg-neutral-900'
+        // console.log(videos)
+        const topBar = newTopBar()
+        const feedElement = newFeedElement(videos)
+        const footer = document.createElement('div')
+        footer.className = 'h-20 bg-white dark:bg-neutral-900'
+        const bottomNavBar = document.createElement('nav')
+        bottomNavBar.className =
+          'absolute w-full p-7 fixed bottom-0 left-0 right-0 lg:hidden z-2 opacity-90 backdrop-blur-md bg-gray-100 dark:bg-neutral-900'
 
-          feedElement.addEventListener('video-click', (event) => {
-            // @ts-ignore: <>
-            const video = event.detail.video
-            history.pushState({}, '', `/video?v=${video.id}&t=${video.time}`)
-            goToPage('/video')
-          })
-
-          topBar.addEventListener('logo-click', (event) => {
-            // history.pushState({}, '', `/`)
-            // goToPage('/')
-          })
-
-          app.innerHTML = ''
-          app.appendChild(topBar)
-          app.appendChild(feedElement)
-          app.appendChild(footer)
-          app.appendChild(bottomNavBar)
+        feedElement.addEventListener('video-click', (event) => {
+          // @ts-ignore: <>
+          const video = event.detail.video
+          history.pushState({}, '', `/video?v=${video.id}&t=${video.time}`)
+          goToPage('/video')
         })
-    })
 
-    dmap.set('/feed', () => {
+        topBar.addEventListener('logo-click', (event) => {
+          // history.pushState({}, '', `/`)
+          // goToPage('/')
+        })
+
+        app.innerHTML = ''
+        app.appendChild(topBar)
+        app.appendChild(feedElement)
+        app.appendChild(footer)
+        app.appendChild(bottomNavBar)
+      })
+  })
+
+  dmap.set('/feed', () => {
+    history.pushState({}, '', `/`)
+    goToPage('/')
+  })
+
+  dmap.set('/video', () => {
+    document.title = 'Narval - Video'
+
+    const urlparameters = new URLSearchParams(globalThis.location.search)
+
+    const id = urlparameters.get('v') || ''
+    const time = urlparameters.get('t') || '0'
+    const video = { id: id, time: time }
+
+    const videoElement = newYoutubeEmbed(video)
+    const topBar = newTopBar()
+    const videoHeader = newVideoHeader(video.id)
+    const footer = document.createElement('div')
+    footer.className = 'h-20 bg-white dark:bg-neutral-900'
+
+    topBar.addEventListener('logo-click', (event) => {
       history.pushState({}, '', `/`)
       goToPage('/')
     })
 
-    dmap.set('/video', () => {
-      document.title = 'Narval - Video'
+    app.innerHTML = ''
+    app.appendChild(topBar)
+    app.appendChild(videoElement)
+    app.appendChild(videoHeader)
+    app.appendChild(footer)
+  })
 
-      const urlparameters = new URLSearchParams(globalThis.location.search)
+  dmap.set('/channel', () => {
+    document.title = 'Narval - Channel'
 
-      const id = urlparameters.get('v') || ''
-      const time = urlparameters.get('t') || '0'
-      const video = { id: id, time: time }
+    const urlparameters = new URLSearchParams(globalThis.location.search)
 
-      const videoElement = newYoutubeEmbed(video)
-      const topBar = newTopBar()
-      const videoHeader = newVideoHeader(video.id)
-      const footer = document.createElement('div')
-      footer.className = 'h-20 bg-white dark:bg-neutral-900'
+    const id = urlparameters.get('c') || ''
 
-      topBar.addEventListener('logo-click', (event) => {
-        history.pushState({}, '', `/`)
-        goToPage('/')
-      })
+    const channelElement = newChannelElement(id)
 
-      app.innerHTML = ''
-      app.appendChild(topBar)
-      app.appendChild(videoElement)
-      app.appendChild(videoHeader)
-      app.appendChild(footer)
-    })
+    // channelElement.addEventListener('logo-click', (event) => {
+    //   history.pushState({}, '', `/`)
+    //   goToPage('/')
+    // })
 
-    dmap.set('/channel', () => {
-      document.title = 'Narval - Channel'
+    app.innerHTML = ''
+    app.appendChild(channelElement)
+  })
 
-      const urlparameters = new URLSearchParams(globalThis.location.search)
-
-      const id = urlparameters.get('c') || ''
-
-      const channelElement = newChannelElement(id)
-
-      // channelElement.addEventListener('logo-click', (event) => {
-      //   history.pushState({}, '', `/`)
-      //   goToPage('/')
-      // })
-
-      app.innerHTML = ''
-      app.appendChild(channelElement)
-    })
-
-    dmap.set('/library', () => {
-      document.title = 'Narval - Library'
-      app.innerHTML = ''
-      app.appendChild(newLibraryElement())
-    })
-  }
+  dmap.set('/library', () => {
+    document.title = 'Narval - Library'
+    app.innerHTML = ''
+    app.appendChild(newLibraryElement())
+  })
 
   const handler = dmap.get(pathname) as () => HTMLElement
   handler()
