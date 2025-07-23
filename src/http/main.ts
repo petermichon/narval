ae7c7d09ff7f5691b04eef68ee4693d67d03e0b3
Peter Michon <peter.michon@proton.me>
2025-07-23 14:22:16 +0200

    fix backend url fetching

--- src/http/main.ts
+++ src/http/main.ts
@@ -1,8 +1,50 @@
-import { serveDir } from 'https://deno.land/std@0.201.0/http/file_server.ts'
+function handler(req: Request): Response {
+  const url = new URL(req.url)
 
-function handler(req: Request) {
-  // return new Response('Hello, World!')
-  return serveDir(req, { fsRoot: './dist', showDirListing: true })
+  if (url.pathname === '/video') {
+    url.pathname = '/'
+  }
+  if (url.pathname === '/feed') {
+    url.pathname = '/'
+  }
+
+  if (url.pathname === '/') {
+    url.pathname = '/index.html'
+  }
+
+  const filePath = `./public${url.pathname}`
+
+  // console.log(url.pathname, ':', filePath)
+
+  let file: Uint8Array<ArrayBuffer>
+  try {
+    file = Deno.readFileSync(filePath)
+  } catch (error) {
+    return new Response('Not Found', { status: 404 })
+  }
+
+  const defaultContentType = 'application/octet-stream'
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+  }
+
+  const fileExtension = filePath.substring(filePath.lastIndexOf('.'))
+
+  const contentType = mimeTypes[fileExtension] || defaultContentType
+
+  return new Response(file, {
+    status: 200,
+    headers: { 'content-type': contentType },
+  })
+
+  // return new Response('Internal Server Error', { status: 500 })
 }
 
 function main() {
@@ -11,17 +53,6 @@ function main() {
 
   const cert = Deno.readTextFileSync(certPath)
   const key = Deno.readTextFileSync(keyPath)
-
-  // const handler = (request: Request) => {
-  //   const body = `${request.headers.get("user-agent")}`;
-  //   const response = new Response(body, {
-  //     headers: {
-  //       "Content-Type": "text/plain; charset=utf-8",
-  //     },
-  //     status: 200,
-  //   });
-  //   return response;
-  // };
 
   const options = { port: 8443, cert: cert, key: key }
 
