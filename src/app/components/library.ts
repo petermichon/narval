e87c904fc61fa40079a86d7eb5a5519b11ed7c90
Peter Michon <peter.michon@proton.me>
2026-01-03 21:12:12 +0100

    improve library page

--- src/app/components/library.ts
+++ src/app/components/library.ts
@@ -1,73 +1,102 @@
 import { newFeedElement } from './feed.ts'
 import { newVideoCardElement } from './video-card.ts'
 
+type Video = { id: string; time: string }
+
 function newLibraryElement(): HTMLElement {
   let libElem = document.createElement('div')
-  // {
-  //   const newData = { name: 'Alice', age: 30 }
-  //   localStorage.setItem('userData', JSON.stringify(newData))
-  // }
-  {
-    type Channel = {
-      id: string
-      videos: { id: string }[]
-    }
 
-    const stringData = localStorage.getItem('channels') || '[]'
-    const channels: Channel[] = JSON.parse(stringData)
-
-    channels.push({
-      id: 'UCEi0DIwuAQNe68OK_1hAN7w',
-      videos: [{ id: 'hFOkrLuf94M' }, { id: 'saVzevTylc4' }],
-    })
-
-    // localStorage.setItem('channels', JSON.stringify(channels))
-
-    console.log(JSON.parse(localStorage.getItem('channels') || '[]'))
-  }
-  {
-    type Video = {
-      id: string
-    }
-
-    const stringData = localStorage.getItem('videos') || '[]'
-    const videos: Video[] = JSON.parse(stringData)
-
-    videos.push({ id: 'saVzevTylc4' })
-    videos.push({ id: 'hFOkrLuf94M' })
-
-    // localStorage.setItem('videos', JSON.stringify(videos))
-
-    console.log(JSON.parse(localStorage.getItem('videos') || '[]'))
-  }
-
-  const videos = JSON.parse(localStorage.getItem('videos') || '[]')
-
+  const storageVideos = localStorage.getItem('videos') || '[]'
+  const videos: Video[] = JSON.parse(storageVideos)
   libElem = newFeedElement(videos)
-
   {
-    const input = document.createElement('input')
-    input.type = 'text'
-    input.placeholder = 'Type something and press Enter'
-    input.className =
-      'w-80 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-100 shadow-sm'
-    input.style = "font-family: 'Roboto', sans-serif"
-
-    input.addEventListener('keydown', (event: KeyboardEvent) => {
-      if (event.key === 'Enter') {
-        console.log(input.value)
-
-        const videoElem = newVideoCardElement({ id: input.value, time: '0' })
-
-        input.value = '' // Clear input after logging
-        libElem.appendChild(videoElem)
-      }
-    })
-
-    libElem.appendChild(input)
+    const importButton = newImportButton()
+    libElem.appendChild(importButton)
+  }
+  {
+    // const addOneButton = newAddOneButton()
+    // libElem.appendChild(addOneButton)
+  }
+  {
+    // const removeAllButton = newRemoveAllButton()
+    // libElem.appendChild(removeAllButton)
   }
 
   return libElem
+}
+
+function newImportButton(): HTMLElement {
+  const input = document.createElement('input')
+  input.type = 'file'
+  input.className =
+    'text-white font-bold px-4 py-1 rounded-3xl bg-neutral-800 hover:bg-neutral-700 cursor-pointer'
+  input.style.fontFamily = "'Roboto', sans-serif"
+  // input.textContent = 'Import'
+
+  input.addEventListener('change', (event: Event) => {
+    const target = event.target as HTMLInputElement
+    const file = target.files?.[0]
+    if (!file) {
+      return
+    }
+
+    const reader = new FileReader()
+
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      const text = event.target!.result as string
+      // console.log('File contents:', text)
+      // Replace videos
+      localStorage.setItem('videos', text)
+    }
+
+    // Optional: handle errors
+    reader.onerror = (event: ProgressEvent<FileReader>) => {
+      console.error('Error reading file')
+    }
+
+    // Read the file as text (you can also use readAsDataURL, readAsArrayBuffer, etc.)
+    reader.readAsText(file)
+  })
+  return input
+}
+
+function newAddOneButton(): HTMLElement {
+  const button = document.createElement('button')
+  button.className =
+    'text-white font-bold px-4 py-1 rounded-3xl bg-neutral-800 hover:bg-neutral-700 cursor-pointer'
+  button.style.fontFamily = "'Roboto', sans-serif"
+  button.textContent = 'Add video'
+  button.addEventListener('click', () => {
+    const videoId = prompt('Enter video ID')
+    if (videoId) {
+      addVideo({ id: videoId, time: '0' })
+    }
+  })
+  return button
+}
+
+function newRemoveAllButton(): HTMLElement {
+  const button = document.createElement('button')
+  button.className =
+    'text-white font-bold px-4 py-1 rounded-3xl bg-neutral-800 hover:bg-neutral-700 cursor-pointer'
+  button.style.fontFamily = "'Roboto', sans-serif"
+  button.textContent = 'Clear videos'
+  button.addEventListener('click', () => {
+    const answer = confirm('Delete all videos ?')
+    if (answer) {
+      localStorage.clear()
+    }
+  })
+  return button
+}
+
+function addVideo(video: { id: string; time: string }) {
+  const storageVideos = localStorage.getItem('videos') || '[]'
+  const videos: Video[] = JSON.parse(storageVideos)
+  videos.push(video)
+  // videos.push({ id: 'hFOkrLuf94M', time: '0' })
+  // videos.push({ id: 'saVzevTylc4', time: '0' })
+  localStorage.setItem('videos', JSON.stringify(videos))
 }
 
 export { newLibraryElement }
